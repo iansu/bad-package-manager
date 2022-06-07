1aedc81ce5766710b7684f84f08b79c59360e82c
Ian Sutherland <ian@iansutherland.ca>
2022-06-07 14:19:51 -0500

    Set up prettier and eslint

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,10 @@
 import test from 'node:test';
 import assert from 'node:assert';
 
-test('synchronous test', (t) => {
+test('synchronous test', () => {
   assert.strictEqual(1, 1);
 });
 
-test('asynchronous test', async (t) => {
+test('asynchronous test', async () => {
   assert.strictEqual(1, 1);
 });
